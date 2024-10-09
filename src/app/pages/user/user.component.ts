8988a89acfb1f27d6b29fead97bb14d69ce3b28b
Gusti <sugiantara.igk@gmail.com>
2024-10-09 14:11:49 +0700

    feat: user table

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
@@ -12,11 +13,18 @@ export class UserComponent {
   private userService = inject(UserService);
 
   isLoading: boolean = true;
+  users: any = [];
 
   ngOnInit() {
-    this.userService.getAllUser().subscribe((user) => {
-      console.log(user);
-      this.isLoading = false;
+    this.userService.getAllUser().subscribe({
+      next: (user) => {
+        console.log(user);
+        this.users = user;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log(error);
+      },
     });
   }
 }
